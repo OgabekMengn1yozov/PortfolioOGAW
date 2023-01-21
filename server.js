2e18420cad5cfe662cff65ca457557413ad0663f
OgabekMengn1yozov <mengniyozovogabek05@gmail.com>
2023-01-21 11:42:49 +0500

    update

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const Express = require("express");
 const Path = require("path");
+const compression = require("compression");
 
 const app = Express();
+
 
 app.use("/", Express.static(Path.join(__dirname, "/src/public")));
 app.set("view engine", "ejs");
 app.set("views", Path.join(__dirname, "src", "views"));
 
+app.use(
+  compression({  
+    level: 6,        
+    threshold: 100 * 1000,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false; 
+      }
+      return compression.filter(req, res);
+    },
+  })
+);
 app.use("/", (req, res) => {
     res.render("index")
 })
 
-app.listen(3000, () => console.log("server run"));
+app.listen(8000, () => console.log("server run 8000"));
